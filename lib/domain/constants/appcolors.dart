8d50e47884d24dc5eda8fc9f8ff492c0b45922a5
Sagar Dhadke <peaceunique2003@gmail.com>
2025-08-02 21:26:22 +0530

    Added Colors

---
+++ lib/domain/constants/appcolors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color scaffoldLight = Color(0XFFFFFFFF);
+  static const Color scaffoldDark = Color(0XFF0F1828);
+  static const Color textLightMode = Color(0XFF0F1828);
+  static const Color textDarkMode = Color(0XFFF7F7FC);
+  static const Color buttonLightMode = Color(0XFF002DE3);
+  static const Color buttonDarkMode = Color(0XFF375FFF);
+  static const Color textfieldLightMode = Color(0XFFF7F7FC);
+  static const Color textfieldDarkMode = Color(0XFF152033);
+  
+}
